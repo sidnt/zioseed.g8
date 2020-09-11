d34c445bf7775a967c0e5cc03aabb982ba868fc4
sidnt <sidnt@users.noreply.github.com>
2020-09-11 20:32:16 +0530

    bump

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    lazy val zioV = "1.0.0-RC20"
+    lazy val zioV = "1.0.1"
   }; import Versions._
 
   lazy val zio = "dev.zio" %% "zio" % zioV
